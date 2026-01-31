f90a4bf28adbbc57456610313a3e1465fe6584d0
mykah10000 <micahluck8@gmail.com>
2026-01-31 16:38:52 -0500

    first commit

---
+++ main_ml.cpp
@@ -0,0 +1,23 @@
+#include "ml.h"
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main(){
+    layer layer0(2,0);
+    layer0.output = {.5,.1};
+    layer layer1;
+    layer1.biases = {.6,.3,.9};
+    layer layer2;
+    layer2.biases = {.2};
+    layer1.weights = set_weights_rand(layer1,layer0);
+    layer1.output = calculate_value(layer1, layer0);
+    layer2.weights = set_weights_rand(layer2, layer1);
+    layer2.output = calculate_value(layer2, layer1);
+    for(float i : layer1.output){
+        cout << i << "\n";
+    }
+    cout << "Final output: " << layer2.output[0];
+    return 0;
+}
---
+++ ml.h
@@ -0,0 +1,58 @@
+#include <vector>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <random>
+using namespace std;
+
+// struct node {
+//     std::vector<float> weights;
+//     float bias;
+//     float output;
+// };
+
+struct layer{
+    vector<vector<float>> weights;
+    vector<float> biases;
+    vector<float> output;
+    layer(){
+
+    }
+    layer(int num_neurons, int num_inputs){
+        weights.resize(num_neurons, vector<float>(num_inputs));
+        biases.resize(num_neurons);
+        output.resize(num_neurons);
+    }
+};
+
+vector<vector<float>> set_weights_rand(layer currentLayer, layer prevLayer){
+    int num_neurons = currentLayer.biases.size();
+    int num_inputs = prevLayer.output.size();
+    vector<vector<float>> output(num_neurons, vector<float>(num_inputs));
+    random_device dev;
+    mt19937 rng(dev());
+    uniform_real_distribution<float> dist(0.0f,1.0f);
+    for(int i = 0; i<currentLayer.biases.size();i++){
+        for(int k = 0; k<prevLayer.output.size();k++){
+            output[i][k] = dist(rng);
+            cout << "Neuron: " << i << ". Weight: " << k << ": " << output[i][k] << "\n";
+        }
+    }
+    return output; 
+}
+
+vector<float> calculate_value(layer currentLayer, layer prevLayer){
+    vector<float> output(currentLayer.biases.size());
+    for(int i = 0; i<currentLayer.biases.size();i++){
+        for(int k = 0; k<prevLayer.output.size();k++){
+            output[i] += (currentLayer.weights[i][k]*prevLayer.output[k]);
+            cout << "Neuron: " << i << ". Weight * prevLayer output " << k << ": " << currentLayer.weights[i][k]*prevLayer.output[k] << "\n";
+        }
+        output[i] += currentLayer.biases[i];
+    }
+    return output;
+}
+
+// vector<layer> take_values(string file_name){
+//     fstream values(file_name);
+// }
