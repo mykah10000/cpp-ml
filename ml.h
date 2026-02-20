f2163a1ba06a05b22f90312332db9cb41b8b63a1
mykah10000 <micahluck8@gmail.com>
2026-02-19 20:02:28 -0500

    messing around with random variables and linear regression

--- ml.h
+++ ml.h
@@ -5,17 +5,35 @@
 #include <random>
 using namespace std;
 
-// struct node {
-//     std::vector<float> weights;
-//     float bias;
-//     float output;
-// };
+struct node {
+    vector<float> weights;
+    float bias;
+    float output;
+};
+
+struct network{
+    private:
+        vector<layer> layers;
+
+    public:
+        void calculate_value(){
+            for(int i = 1; i<layers.size();i++){
+                for(int p = 0; p<layers[i].checkSize();p++){
+                    for(int k = 0; k<layers[i-1].checkSize();k++){
+                        output[i] += (currentLayer.weights[i][k]*prevLayer.output[k]);
+                        cout << "Neuron: " << i << ". Weight * prevLayer output " << k << ": " << currentLayer.weights[i][k]*prevLayer.output[k] << "\n";
+                    }
+                output[i] += currentLayer.biases[i];
+                }
+            }
+        }
+};
 
 struct layer{
-    vector<vector<float>> weights;
-    vector<float> biases;
-    vector<float> output;
-    float cost;
+        vector<vector<float>> weights;
+        vector<float> biases;
+        vector<float> output;
+        float cost;
         
     public:
         layer(){}
@@ -26,33 +44,25 @@ struct layer{
             output.resize(num_neurons);
         }
 
-        vector<vector<float>> set_weights_rand(layer currentLayer, layer prevLayer){
-        int num_neurons = currentLayer.biases.size();
+        void set_weights_rand(layer prevLayer){
+        int num_neurons = biases.size();
         int num_inputs = prevLayer.output.size();
         vector<vector<float>> output(num_neurons, vector<float>(num_inputs));
         random_device dev;
         mt19937 rng(dev());
         uniform_real_distribution<float> dist(0.0f,1.0f);
-        for(int i = 0; i<currentLayer.biases.size();i++){
+        for(int i = 0; i<biases.size();i++){
             for(int k = 0; k<prevLayer.output.size();k++){
-                output[i][k] = dist(rng);
+                weights[i][k] = dist(rng);
                 cout << "Neuron: " << i << ". Weight: " << k << ": " << output[i][k] << "\n";
             }
         }
-        return output; 
+        }
+        int checkSize(){
+            return output.size();
         }
 
-        vector<float> calculate_value(layer currentLayer, layer prevLayer){
-        vector<float> output(currentLayer.biases.size());
-        for(int i = 0; i<currentLayer.biases.size();i++){
-            for(int k = 0; k<prevLayer.output.size();k++){
-                output[i] += (currentLayer.weights[i][k]*prevLayer.output[k]);
-                cout << "Neuron: " << i << ". Weight * prevLayer output " << k << ": " << currentLayer.weights[i][k]*prevLayer.output[k] << "\n";
-            }
-            output[i] += currentLayer.biases[i];
-        }
-        return output;
-}
+        
 };
 
 
--- ml_test.cpp
+++ ml_test.cpp
@@ -1,4 +1,9 @@
-#include "ml.h"
+// #include "ml.h"
+#include <vector>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <random>
 using namespace std;
 
 float forward(float activation, float weight, float bias){
@@ -12,21 +17,58 @@ float forward(float activation, float weight, float bias){
 // }
 
 int main(){
-    float x = 2;
-    float y = 4;
-    float b = .5;
-    float w = .5;
-    float y_hat = forward(w,x,b);
-    float l = pow(.5 * (y_hat-y),2);
-    float error = y_hat-y;
-    cout << "init error: " << error <<"\n";
-    for(int i = 0; i < 100;i++){
-        y_hat = forward(w,x,b);
-        float gradient = ((y_hat-y)*x);
-        w = (w-.1*gradient);
+    const int n = 100;
+    vector<float> X(n);
+    vector<float> y(n);
+    
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_real_distribution<> uniform_dist(-10.0,10.0);
+    uniform_real_distribution<> randSlope(-10.0,10.0);
+    uniform_real_distribution<> randIntercept(-20.0,20.0);
+    normal_distribution<> noise_dist(0.0,1.0);
+
+    float slope = randSlope(gen);
+    float intercept = randIntercept(gen);
+
+    for(int i = 0;i<n;i++){
+        float x = uniform_dist(gen);
+        float noise = noise_dist(gen);
+
+        X[i] = x;
+        y[i] = slope * x + intercept + noise;
+        // cout << y[i] << "\n";
     }
-    error = y_hat-y;
-    cout << "End weight: " << w << "\nEnd error: "<< error;
+    float w = 0;
+    float b = 0;
+    float loss = 0;
+    float error;
+    float y_hat = forward(X[0],w,b);
+    float learningRate = .01;
+    
+    cout << "init error: " << y[0]-y_hat;
+    cout << " init w: " << w << ", init b: " << b << "\n";
+    for(int epoch = 0; epoch<1000;epoch++){
+        double dw = 0;
+        double db = 0;
+        double loss = 0;
+
+        for(int k = 0;k<X.size();k++){
+            y_hat = forward(X[k],w,b);
+            error = y[k]-y_hat;
+            loss += error * error;
+            db += -2*error;
+            dw += -2*error*X[k];
+        }
+        dw = dw/n;
+        db = db/n;
+        loss /= n;
+        w -= learningRate*dw;
+        b -= learningRate*db;
+        cout << "Epoch " << epoch << " error: " << error << ", w: "<< w << ", b: " << b << "\n";
+    }
+    cout << "End error: " << error << ", End w: "<< w << ", End b: " << b << "\n";
+    cout << "Slope: " << slope << ", Intercept: " << intercept;
 }
 
 
